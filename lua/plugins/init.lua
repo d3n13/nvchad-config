8bdccf5ce060e535c64f7b19134e66b8953c9e0a
Denis Bilenko <goldenrodcap@gmail.com>
2025-08-03 20:36:05 +0400

    feat: git inline blame

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,25 +1,21 @@
-return {
-  {
+return {{
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
-
-  -- These are some examples, uncomment them if you want to see them work!
-  {
+    opts = require "configs.conform"
+}, {
     "neovim/nvim-lspconfig",
     config = function()
-      require "configs.lspconfig"
-    end,
-  },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
-}
+        require "configs.lspconfig"
+    end
+}, {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+        current_line_blame = true,
+        current_line_blame_opts = {
+            virt_text = true,
+            virt_text_pos = "eol",
+            delay = 300,
+            ignore_whitespace = false
+        }
+    }
+}}
